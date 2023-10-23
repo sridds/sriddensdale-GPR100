e63c647fdaee0d889af888bd4d5a70c9c7612894
sridds <seth.riddensdale@mymail.champlain.edu>
2023-10-22 22:11:03 -0400

    Smooth Collectable Movement
    For some more indication of the general direction the collectable moved in, the collectables movement has been smoothed out.

--- GPR-TestProject1/Assets/Scripts/Collectable.cs
+++ GPR-TestProject1/Assets/Scripts/Collectable.cs
@@ -6,8 +6,13 @@ public class Collectable : MonoBehaviour
 {
     [SerializeField] private Vector2 moveRange;
     [SerializeField] private int pointValue = 50;
+    [SerializeField] private float smoothTime = 0.2f;
 
     private Vector2 startPos;
+    private Vector2 targetPos;
+    private Vector2 velocity;
+
+    private bool canCollect = true;
 
     public delegate void OnTouch(int points);
     public OnTouch OnTouchEvent;
@@ -15,20 +20,32 @@ public class Collectable : MonoBehaviour
     private void Start()
     {
         startPos = transform.position;
+        targetPos = transform.position;
+    }
+
+    private void Update()
+    {
+        // smooth position to target
+        transform.position = Vector2.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+
+        if (Vector2.Distance(transform.position, targetPos) < 0.5f) canCollect = true;
+        else canCollect = false;
     }
 
     void Touch()
     {
         // Randomize position
-        transform.position = new Vector2(
+        targetPos = new Vector2(
             startPos.x + Random.Range(-moveRange.x, moveRange.x),
             startPos.y + Random.Range(-moveRange.y, moveRange.y));
 
         OnTouchEvent?.Invoke(pointValue);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
+        if (!canCollect) return;
+
         // If the player touches the collectable, perform the Touch() function
         if (collision.GetComponent<TopDownMovement>())
         {
