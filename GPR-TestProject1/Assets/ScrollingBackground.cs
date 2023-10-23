cfbd39292bcdbf3f1352b804b16ffe4d4577238e
sridds <seth.riddensdale@mymail.champlain.edu>
2023-10-22 23:02:30 -0400

    Scrolling Background
    Added a scrolling background to the game

---
+++ GPR-TestProject1/Assets/ScrollingBackground.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] private Vector2 scrollSpeed;
+    [SerializeField] private RawImage image;
+
+    private void Update()
+    {
+        image.material.mainTextureOffset = new Vector2(image.material.mainTextureOffset.x + scrollSpeed.x * Time.deltaTime, image.material.mainTextureOffset.y + scrollSpeed.y * Time.deltaTime);
+
+    }
+}
